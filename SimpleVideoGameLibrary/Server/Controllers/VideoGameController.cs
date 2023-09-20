c6685b27d75ea8ebd758a3a6e2084a8e2c021a4f
Finels <francisco.bonitos@gmail.com>
2023-09-20 15:30:49 +0100

    Add a GET method

--- SimpleVideoGameLibrary/Server/Controllers/VideoGameController.cs
+++ SimpleVideoGameLibrary/Server/Controllers/VideoGameController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SimpleVideoGameLibrary.Shared;
 
 namespace SimpleVideoGameLibrary.Server.Controllers
 {
@@ -7,5 +8,15 @@ namespace SimpleVideoGameLibrary.Server.Controllers
     [ApiController]
     public class VideoGameController : ControllerBase
     {
+        public async Task<ActionResult<List<VideoGame>>> GetAllVideoGames()
+        {
+            var videoGames = new List<VideoGame>()
+            {
+                new VideoGame { Id = 1, Name ="Tetris", Publisher = "Nintendo", ReleaseYear = 1989},
+                new VideoGame { Id = 2, Name ="Pong", Publisher = "Atari", ReleaseYear = 1972}
+            };
+
+            return Ok(videoGames);
+        }
     }
 }
