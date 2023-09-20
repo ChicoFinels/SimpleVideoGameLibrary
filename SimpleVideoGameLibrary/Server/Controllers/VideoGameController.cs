3cfec43ba7479ab0889b98aa5bb2c6cc96fd560a
Finels <francisco.bonitos@gmail.com>
2023-09-20 18:05:59 +0100

    Implement updating a video game

--- SimpleVideoGameLibrary/Server/Controllers/VideoGameController.cs
+++ SimpleVideoGameLibrary/Server/Controllers/VideoGameController.cs
@@ -19,9 +19,48 @@ namespace SimpleVideoGameLibrary.Server.Controllers
 
 		public async Task<ActionResult<List<VideoGame>>> GetAllVideoGames()
 		{
-			var videoGames = await _context.VideoGames.OrderBy(g=>g.ReleaseYear).ToListAsync();
+			var videoGames = await _context.VideoGames.OrderBy(g => g.ReleaseYear).ToListAsync();
 
 			return Ok(videoGames);
+		}
+
+		[HttpGet("{id}")]
+		public async Task<ActionResult<VideoGame>> GetVideoGame(int id)
+		{
+			var dbGame = await _context.VideoGames.FindAsync(id);
+			if (dbGame == null)
+			{
+				return NotFound("This video game doesn't exist!");
+			}
+
+			return Ok(dbGame);
+		}
+
+		[HttpPost]
+		public async Task<ActionResult<List<VideoGame>>> CreateVideoGame(VideoGame game)
+		{
+			_context.VideoGames.Add(game);
+			await _context.SaveChangesAsync();
+
+			return await GetAllVideoGames();
+		}
+
+		[HttpPut("{id}")]
+		public async Task<ActionResult<List<VideoGame>>> UpdateVideoGame(int id, VideoGame game)
+		{
+			var dbGame = await _context.VideoGames.FindAsync(id);
+			if (dbGame == null)
+			{
+				return NotFound("This video game doesn't exist!");
+			}
+
+			dbGame.Name = game.Name;
+			dbGame.Publisher = game.Publisher;
+			dbGame.ReleaseYear = game.ReleaseYear;
+
+			await _context.SaveChangesAsync();
+
+			return await GetAllVideoGames();
 		}
 	}
 }
